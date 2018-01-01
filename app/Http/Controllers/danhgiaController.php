da6ad588764fe1c5ca347391a8da07eb94761d76
huy7644 <32673667+huy7644@users.noreply.github.com>
2018-01-01 20:29:05 +0700

    fix danhgia/id

--- app/Http/Controllers/danhgiaController.php
+++ app/Http/Controllers/danhgiaController.php
@@ -37,7 +37,8 @@ class danhgiaController extends Controller
     public function show($id_dich_vu)
     {
         $danh_gia = DB::table('dlct_danhgia')
-        ->select('dlct_nguoidung.nd_tendangnhap','dg_diem','dg_noidung', 'dg_tieude')
+        ->select('dlct_nguoidung.nd_tendangnhap','dg_diem','dg_noidung', 'dg_tieude', 
+            DB::raw('DATE_FORMAT(dlct_danhgia.created_at,"%d-%m-%Y") as ngaydanhgia'))
         ->join('dlct_nguoidung', 'dlct_danhgia.nd_idnguoidung', '=', 'dlct_nguoidung.id')
         ->where('dlct_danhgia.dv_iddichvu', $id_dich_vu)
         ->paginate(10);
