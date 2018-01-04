75daba0baa8aaa2d81ff70db2fbf39a1ac8ef6f6
js1smiller <32253940+js1smiller@users.noreply.github.com>
2018-01-04 15:13:34 +0700

    danhgiaController

--- app/Http/Controllers/danhgiaController.php
+++ app/Http/Controllers/danhgiaController.php
@@ -34,14 +34,21 @@ class danhgiaController extends Controller
         $danhgia->save();
     }
 
-    public function show($id_dich_vu)
+    public function show($id)
     {
+        // $danh_gia = DB::table('dlct_danhgia')
+        // ->select('dlct_nguoidung.nd_tendangnhap','dg_diem','dg_noidung', 'dg_tieude', 
+        //     DB::raw('DATE_FORMAT(dlct_danhgia.created_at,"%d-%m-%Y") as ngaydanhgia'))
+        // ->join('dlct_nguoidung', 'dlct_danhgia.nd_idnguoidung', '=', 'dlct_nguoidung.id')
+        // ->where('dlct_danhgia.dv_iddichvu', $id_dich_vu)
+        // ->paginate(10);
+        // $encode=json_encode($danh_gia);
+        // return $encode;
+
         $danh_gia = DB::table('dlct_danhgia')
-        ->select('dlct_nguoidung.nd_tendangnhap','dg_diem','dg_noidung', 'dg_tieude', 
-            DB::raw('DATE_FORMAT(dlct_danhgia.created_at,"%d-%m-%Y") as ngaydanhgia'))
-        ->join('dlct_nguoidung', 'dlct_danhgia.nd_idnguoidung', '=', 'dlct_nguoidung.id')
-        ->where('dlct_danhgia.dv_iddichvu', $id_dich_vu)
-        ->paginate(10);
+        ->select('dg_tieude', 'dg_noidung', 'dg_diem')
+        ->where('id', $id)
+        ->get();
         $encode=json_encode($danh_gia);
         return $encode;
     }
