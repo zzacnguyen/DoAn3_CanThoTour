7c1046f4bd333794c90309f3a624a1c9c7c0efb3
tinhphan1 <pvtinh.software@gmail.com>
2018-03-07 14:28:13 +0700

    post

--- app/Http/Controllers/tourist_places_Controller.php
+++ app/Http/Controllers/tourist_places_Controller.php
@@ -23,31 +23,10 @@ class tourist_places_controller extends Controller
         $id_place = $convert['id'];
         return $id_place;		
 	}
-    function AddServices()
-    {
-        $vnt_services     = array (new servicesModel);
-        $vnt_services->sv_description   = $request->input('sv_description');
-        $vnt_services->sv_open    = $request->input('sv_open');
-        $vnt_services->sv_close  = $request->input('sv_close');
-        $vnt_services->sv_highest_price  = $request->input('sv_highest_price');
-        $vnt_services->sv_lowest_price = $request->input('sv_lowest_price');
-        $vnt_services->sv_phone_number   = $request->input('sv_phone_number');
-        $vnt_services->sv_status   = "Active";
-        $vnt_services->sv_types   = $request->input('sv_types');
-        $vnt_services->tourist_places_id   =$id_place;
-        $vnt_services->save();
-        $lastServices = DB::table('vnt_services')->orderBy('id', 'desc')->first();
-        $convert = (array)$lastServices;
-        $id_service =  $convert['id'];
-        $id_type =  $convert['sv_types'];
-    }
-    
 
     public function AddPlace(Request $request, $number_types)
 	{
 		$place             		= new touristPlacesModel;
-
-
         $place->pl_name   		= $request->input('pl_name');
         $place->pl_details 		= $request->input('pl_details');
         $place->pl_address 		= $request->input('pl_address');
@@ -58,96 +37,6 @@ class tourist_places_controller extends Controller
         $place->user_id 		= $request->input('user_id');
         $place->save();
         $id_place = $this::GetIDLastPlace();
-        $vnt_services     = array (new servicesModel);
-        $vnt_services->sv_description   = $request->input('sv_description');
-        $vnt_services->sv_open    = $request->input('sv_open');
-        $vnt_services->sv_close  = $request->input('sv_close');
-        $vnt_services->sv_highest_price  = $request->input('sv_highest_price');
-        $vnt_services->sv_lowest_price = $request->input('sv_lowest_price');
-        $vnt_services->sv_phone_number   = $request->input('sv_phone_number');
-        $vnt_services->sv_status   = "Active";
-        $vnt_services->sv_types   = $request->input('sv_types');
-        $vnt_services->tourist_places_id   =$id_place;
-        $vnt_services->save();
-        $lastServices = DB::table('vnt_services')->orderBy('id', 'desc')->first();
-        $convert = (array)$lastServices;
-        $id_service =  $convert['id'];
-        $id_type =  $convert['sv_types'];
-        for($i = 0; $i < $number_types; $i++)
-        {
-            $lastServices = DB::table('vnt_services')->orderBy('id', 'desc')->first();
-            $convert = (array)$lastServices;
-            $id_service =  $convert['id'];
-            $id_type =  $convert['sv_types'];
-            $tmp_phone = $request->input('sv_phone_number');
-            $phoneServices = DB::table('vnt_services')->where("sv_phone_number","=",$tmp_phone)->get();
-            $tmp_types   = $request->input('sv_types');
-            $convert2 = $phoneServices;
-            $tmp = count($convert2);
-                
-            if($tmp == 0)
-            {
-                
-
-                if($id_type == 1)
-                {
-                    $vnt_services[]                 = new servicesModel;
-                    $vnt_services->sv_description   = $request->input('sv_description');
-                    $vnt_services->sv_open    = $request->input('sv_open');
-                    $vnt_services->sv_close  = $request->input('sv_close');
-                    $vnt_services->sv_highest_price  = $request->input('sv_highest_price');
-                    $vnt_services->sv_lowest_price = $request->input('sv_lowest_price');
-                    $vnt_services->sv_phone_number   = $request->input('sv_phone_number');
-                    $vnt_services->sv_status   = "Active";
-                    $vnt_services->sv_types   = $request->input('sv_types');
-                    $vnt_services->tourist_places_id   =$id_place;
-                    $vnt_services->save();
-                    $lastServices = DB::table('vnt_services')->orderBy('id', 'desc')->first();
-                    $convert = (array)$lastServices;
-                    $id_service =  $convert['id'];
-                    $id_type =  $convert['sv_types'];
-                    $vnt_eating = new eatingModel;
-                    $vnt_eating->eat_name =  $request->input('eat_name');
-                    $vnt_eating->eat_status =  "Active";
-                    $vnt_eating->service_id =  $id_service;
-                    $vnt_eating->save();
-                }
-                if($id_type == 2)
-                {
-                    $vnt_hotels = new hotelsModel;
-                    $vnt_hotels->hotel_name =  $request->input('hotel_name');
-                    $vnt_hotels->hotel_website =  $request->input('hotel_website');
-                    $vnt_hotels->hotel_number_star =  $request->input('hotel_number_star');
-                    $vnt_hotels->hotel_status =  "Active";
-                    $vnt_hotels->service_id =  $id_service;
-                    $vnt_hotels->save();
-                }
-                if($id_type == 3)
-                {
-                    $vnt_transport = new transportModel;
-                    $vnt_transport->transport_name =  $request->input('transport_name');
-                    $vnt_transport->transport_status =  "Active";
-                    $vnt_transport->service_id =  $id_service;
-                    $vnt_transport->save();
-                }
-                if($id_type == 4)
-                {
-                    $vnt_sightseeing = new sightseeingModel;
-                    $vnt_sightseeing->sightseeing_name =  $request->input('sightseeing_name');
-                    $vnt_sightseeing->sightseeing_status     =  "Active";
-                    $vnt_sightseeing->service_id =  $id_service;
-                    $vnt_sightseeing->save();
-                }
-                if($id_type == 5)
-                {
-                    $vnt_entertainments = new sightseeingModel;
-                    $vnt_entertainments->entertainments_name =  $request->input('entertainments_name');
-                    $vnt_entertainments->entertainments_status       =  "Active";
-                    $vnt_entertainments->service_id =  $id_service;
-                    $vnt_entertainments->save();
-                }
-            }
-            
-        } 
+        return json_encode($id_place);
 	}
 }
