2f32ba161ff971ec4c821e359678adf9309d77b7
tinhphan1 <pvtinh.software@gmail.com>
2018-03-15 19:38:55 +0700

    fix likesController

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -14,7 +14,18 @@ class LikeController extends Controller
      */ 
     public function index()
     {
-        //
+        $like = DB::table('vnt_likes')
+        ->select('vnt_likes.id', 'vnt_likes.user_id', 'vnt_images.id AS image_id','vnt_images.image_details_1')
+        ->leftJoin('vnt_images', 'vnt_images.service_id', '=', 'vnt_likes.service_id')
+        ->leftJoin('vnt_hotels', 'vnt_hotels.service_id', '=', 'vnt_likes.service_id')
+        ->leftJoin('vnt_eating', 'vnt_eating.service_id', '=', 'vnt_likes.service_id')
+        ->leftJoin('vnt_entertainments', 'vnt_entertainments.service_id', '=', 'vnt_likes.service_id')
+        ->leftJoin('vnt_sightseeing', 'vnt_sightseeing.service_id', '=', 'vnt_likes.service_id')
+        ->leftJoin('vnt_transport', 'vnt_transport.service_id', '=', 'vnt_likes.service_id')
+        ->join('vnt_services', 'vnt_services.id', '=', 'vnt_likes.service_id')
+        ->paginate (10);
+        $encode=json_encode($like);
+        return $encode;
     }
 
     /**
@@ -24,7 +35,11 @@ class LikeController extends Controller
      */
     public function create()
     {
-        //
+        $likes                    = new likesModel();
+        $likes->service_id       = $request->input('service_id');
+        $likes->user_id    = $request->input('user_id');
+        $likes->save();
+
     }
 
     /**
@@ -69,7 +84,12 @@ class LikeController extends Controller
      */
     public function edit($id)
     {
-        //
+        $likes = DB::table('vnt_likes')
+        ->select('id','vnt_services', 'vnt_likes')
+        ->where('id',$id)
+        ->get();
+        $encode=json_encode($likes);
+        return $encode;
     }
 
     /**
@@ -81,7 +101,11 @@ class LikeController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $likes                      = likesModel::findOrFail($id);
+        $likes->service_id        = $request->input('service_id');
+        $likes->user_id      = $request->input('user_id');
+        $likes->save();
+
     }
 
     /**
@@ -92,6 +116,7 @@ class LikeController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $likes = likesModel::findOrFail($id);
+        $likes->delete();
     }
 }
