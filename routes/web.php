f4b1b6d184e3f582d0d7c2477832d581ab031f87
tinhphan1 <pvtinh.software@gmail.com>
2018-04-08 23:36:27 +0700

    Partner_count_posted_for_month

--- routes/web.php
+++ routes/web.php
@@ -14,76 +14,34 @@ include 'cms.php';
 
 
 Route::resource('eating', 'EatingController');
-//thay thế
-Route::resource('anuong', 'anuongController');
 
-//Bỏ
-Route::resource('binhluan', 'binhluanController');
 
-//Chưa sử dụng
-Route::resource('chitietlichtrinh', 'chitietlichtrinhController');
 
 Route::resource('visitor-ratings', 'VisitorRatingController');
-//thay thế
-Route::resource('danhgia', 'danhgiaController');
+
 
 Route::resource('service','ServicesController');
-//thay thế
 Route::resource('usersearch', 'userSearch');
 
 Route::resource('hotels', 'vnt_hotelsController');
-Route::resource('khachsan', 'khachsanController');
-
-Route::resource('lichtrinh', 'lichtrinhController');
-
-
 Route::post('add-places', 'tourist_places_controller@AddPlace');
 Route::post('add-services/{id}','tourist_places_controller@AddServices');
 Route::get('get-name-services/{id}', 'tourist_places_controller@GetNamePlace');
 Route::resource('tourist-places', 'tourist_places_Controller');
-Route::resource('diadiem', 'diadiemController');
-
-Route::resource('loaihinhsukien', 'loaihinhsukienController');
-
-Route::resource('nguoidung', 'nguoidungController');
-
 Route::resource('transport', 'transportController');
-//thay thế
-Route::resource('phuongtien', 'phuongtienController');
 
+//sự kiện
 Route::resource('events', 'EventsController');
 Route::get('counter-events', 'CounterEventsController@countEvent');
-
-Route::resource('sukien', 'sukienController');
-
-
-
+//tham quan
 Route::resource('sightseeing', 'sightseeingController');
-//thay thế
-Route::resource('thamquan', 'thamquanController');
 
 Route::resource('entertainments', 'vnt_entertainmentsController');
-Route::resource('vuichoi', 'vuichoiController');
-
-Route::resource('tukhoa', 'tukhoaController');
-
-Route::resource('tukhoa_dichvu', 'tukhoa_dichvu_Controller');
-
 Route::resource('like', 'LikeController');
-//thay thế
-Route::resource('yeuthich', 'yeuthichController');
-
 
 // TIM KIEM
 // tìm kiếm địa điểm lân cận giới hạn 10 địa điểm
-Route::get('timkiem/diadiemlancan/location={latitude},{longitude}&radius={radius}','timkiemController@search_lancan');
 
-Route::get('timkiem/dichvulancan/location={latitude},{longtitude}&type={type}&radius={radius}','timkiemController@search_dichvu_lancan');
-Route::get('timkiem/dichvu/id={iddiadiem}','timkiemController@get_dichvu');
-
-Route::get('timkiem/dichvuall/keyword={keyword}','timkiemController@search_dichvu_all');
-
-Route::get('timkiem/dichvunangcao/idtype={type}&keyword={keyword}','timkiemController@search_dichvu_type');
 
 // search new
 Route::get('search/placevicinity/location={latitude},{longitude}&radius={radius}','SearchController@searchPlaceVicinity');
@@ -112,8 +70,13 @@ Route::get('get-detail-1/{id}', 'ImagesController@getImageDetail1');
 Route::get('get-detail-2/{id}', 'ImagesController@getImageDetail2');
 Route::get('get-only-icon-image', 'ImagesController@GetOnlyIconImage');
 Route::get('rating-service/{id}','Rating_Service_Controller@rating');
-//thay-the
-Route::get('danhgia-dichvu/{id}','danhgia_dichvu_controller@danhgia');
+
 Route::get('google-maps','testGoogleMapsApi@FunctionName');
 
+
+
+
+//partner
+Route::get('get-services-posted-partner/month={month}&user_id={id}','Partner_Controller@getServices');
+Route::get('get-places-posted-partner/month={month}&user_id={id}','Partner_Controller@getServices');
 
