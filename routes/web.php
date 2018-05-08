7cf46f46a3aa9181f620cf6041f2048de94a71cc
huy7644 <32673667+huy7644@users.noreply.github.com>
2018-05-08 10:24:46 +0700

    cập nhật csdl + sửa đăng ký đăng nhập + putrating

--- routes/web.php
+++ routes/web.php
@@ -50,8 +50,8 @@ Route::get('search/services/keyword={keyword}','SearchController@searchServicesK
 Route::get('search/searchServicesTypeKeyword/type={type}&keyword={keyword}','SearchController@searchServicesTypeKeyword');
 
 // LOGIN-LOGOUT-REGISTER
-Route::post('login2','loginController@postLogin');
-Route::post('register','loginController@register');
+Route::post('login-mobile','loginController@postLogin')->name('login-mobile');
+Route::post('register-mobile','loginController@register')->name('register-mobile');
 Route::get('logout','loginController@logout');
 // web
 
