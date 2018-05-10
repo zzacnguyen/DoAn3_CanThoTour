fb71cfc9458a8a7e084a1f7e411327ae338b61f7
huy7644 <32673667+huy7644@users.noreply.github.com>
2018-05-10 20:57:09 +0700

    sửa lịch trình

--- app/Http/Controllers/tripScheduleController.php
+++ app/Http/Controllers/tripScheduleController.php
@@ -21,13 +21,8 @@ class tripScheduleController extends Controller
     }
     public function getDetailTripSchedule($id)
     {
-        $schedule = DB::table('vnt_tripschedule')
-        ->select('id', 'trip_name', 'trip_startdate', 'trip_enddate')
-        ->orderBy('id', 'DESC')
-        ->where('vnt_tripschedule.id', '=', $id)
-        ->get();
-
         $list_services = DB::table('vnt_tripschedule_details')
+        ->leftJoin('vnt_images', 'vnt_images.service_id', '=', 'vnt_tripschedule_details.service_id')
         ->leftJoin('vnt_services', 'vnt_services.id', '=', 'vnt_tripschedule_details.service_id' )
         ->leftJoin('vnt_tripschedule', 'vnt_tripschedule.id', '=', 'vnt_tripschedule_details.trip_id')
         ->leftJoin('vnt_hotels', 'vnt_hotels.service_id', '=', 'vnt_services.id')
@@ -35,12 +30,11 @@ class tripScheduleController extends Controller
         ->leftJoin('vnt_entertainments', 'vnt_entertainments.service_id', '=', 'vnt_services.id')
         ->leftJoin('vnt_sightseeing', 'vnt_sightseeing.service_id', '=', 'vnt_services.id')
         ->leftJoin('vnt_transport', 'vnt_transport.service_id', '=', 'vnt_services.id')
-        ->select('vnt_services.id', 'hotel_name', 'sightseeing_name', 'entertainments_name', 'transport_name', 'eat_name' )
+        ->select('vnt_services.id', 'hotel_name', 'sightseeing_name', 'entertainments_name', 'transport_name', 'eat_name', 'vnt_images.id AS image_id','vnt_images.image_details_1')
         ->where('vnt_tripschedule_details.trip_id', '=', $id)
-        ->get();
-
-        $merge[] = ["schedule"=>$schedule, "list_services"=>$list_services];  
-        $encode=json_encode($merge);
+        ->paginate(10);
+  
+        $encode=json_encode($list_services);
         return $encode;
     }
     public function postTripSchedule(Request $request, $user_id)
@@ -49,16 +43,10 @@ class tripScheduleController extends Controller
     	$schedule->trip_name = $request->input('trip_name')  ;
     	$schedule->trip_startdate = $request->input('trip_startdate');
     	$schedule->trip_enddate = $request->input('trip_enddate');
-    	$schedule->service_id = $request->input('service_id');
     	$schedule->user_id = $user_id;
     	if($schedule->save())
         {
-	        $tripSchedule = DB::table('vnt_tripschedule')
-	    	->select('id')
-	    	->orderBy('id', 'DESC')
-	    	->limit(1)
-            ->get();
-            return json_encode($tripSchedule);
+            return json_encode("status:200");
         }
         else{
             return json_encode("status:500");
